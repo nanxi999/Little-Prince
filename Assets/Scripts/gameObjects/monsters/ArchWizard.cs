b7f26efddb89ea031d8ce8418756355093ffcea4
nali9306 <nali9306@uni.sydney.edu.au>
2021-07-07 16:56:29 +1000

    Adds nick scene

--- Assets/Scripts/gameObjects/monsters/ArchWizard.cs
+++ Assets/Scripts/gameObjects/monsters/ArchWizard.cs
@@ -10,38 +10,33 @@ public class ArchWizard : Enemy
     public float attackCd;
     private float sinceLastAttack;
 
-    protected override void Start()
-    {
-        prince = FindObjectOfType<Prince>();
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponentInChildren<Animator>();
-    }
-
     protected override void Update()
     {
         sinceLastAttack += Time.deltaTime;
         base.Update();
+        transform.rotation = Quaternion.EulerRotation(-transform.rotation.x, transform.rotation.y, transform.rotation.z);
         FireTrigger();
     }
 
     public void FireTrigger()
     {
-        if (sinceLastAttack < attackCd) { return; }
-        else
+        if (Vector2.Distance(transform.position, prince.transform.position) < attackRange)
         {
-            sinceLastAttack = 0;
-            StartCoroutine(Freeze(1));
-            animator.SetTrigger("Cast");
+            if (sinceLastAttack < attackCd) { return; }
+            else
+            {
+                sinceLastAttack = 0;
+                StartCoroutine(Freeze(1));
+                animator.SetTrigger("Cast");
+            }
         }
     }
-
-
 
     public void Fire()
     {
         if (darkBullet)
         {
-            Vector3 dir = prince.transform.position - transform.position;
+            Vector3 dir = prince.transform.position - attackPoint.transform.position;
             float angle = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg - 90f;
             DarkBullet db = Instantiate(darkBullet, attackPoint.transform.position, transform.rotation);
             db.SetDirection(dir);
@@ -49,10 +44,5 @@ public class ArchWizard : Enemy
             db.SetDmg(dmg);
             db.SetShooter(gameObject);
         }
-    }
-
-    public override void PushBack(Vector3 pushBackVelocity, float duration)
-    {
-        
     }
 }
