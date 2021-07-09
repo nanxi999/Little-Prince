7bdf7436e0e26d7d2fe1698a2ad2296c2c3e0ecf
aliu4429 <aliu4429@uni.sydney.edu.au>
2021-07-09 20:00:12 +1000

    completed award system

--- Assets/AwardsSpawner.cs
+++ Assets/AwardsSpawner.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class AwardsSpawner : MonoBehaviour
 {
     public Award[] awardPrefabArray;
+    public GameObject spawnEffect;
 
     private void Start()
     {
-        SpawnAwards();
+        GetComponent<Animator>().SetTrigger("Spawn");
     }
 
     public void SpawnAwards()
     {
-        Award award1 = Instantiate(GetRandomAward(), transform.position, transform.rotation);
-        Award award2 = Instantiate(GetRandomAward(), transform.position, transform.rotation);
-        Award award3 = Instantiate(GetRandomAward(), transform.position, transform.rotation);
+        Vector3 offset = new Vector3(0, -1, 0);
+        GameObject effect = Instantiate(spawnEffect, transform.position, transform.rotation);
+        Award award1 = Instantiate(GetRandomAward(), transform.position + offset, transform.rotation);
+        Award award2 = Instantiate(GetRandomAward(), transform.position + offset, transform.rotation);
+        Award award3 = Instantiate(GetRandomAward(), transform.position + offset, transform.rotation);
 
         award1.GetComponent<Rigidbody2D>().velocity = new Vector3 (-6f, 5f, 0f);
         award2.GetComponent<Rigidbody2D>().velocity = new Vector3(0f, 5f, 0f);
         award3.GetComponent<Rigidbody2D>().velocity = new Vector3(6f, 5f, 0f);
+
+        Destroy(effect, 2);
     }
 
     private Award GetRandomAward()
